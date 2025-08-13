68e474473761316cd11c5c3a2626b56e8f3dbea8
geha <ospac111@gmail.com>
2025-08-13 17:49:51 +0900

    product 페이지 api 연결(product, comments) 및 date util함수 정의

--- src/pages/Product/components/InfoSection.jsx
+++ src/pages/Product/components/InfoSection.jsx
@@ -1,53 +1,62 @@
+import { useParams } from 'react-router-dom';
+
 import LikeIcon from '@/assets/icons/ic_heart.svg';
 import KebabIcon from '@/assets/icons/ic_kebab.svg';
 import ItemImg from '@/components/ui/ItemImg';
+import Loading from '@/components/ui/Loading';
 import Tag from '@/components/ui/Tag';
+import { useQuery } from '@/hooks/useFetch';
 import AuthorInfo, {
   AUTHOR_INFO_VARIANTS,
 } from '@/pages/Product/components/AuthorInfo';
+import { getProduct } from '@/pages/Product/lib/api';
 import styles from '@/pages/Product/styles/InfoSection.module.scss';
+import { getFormattedDate } from '@/utils/Date';
 
 export default function InfoSection() {
+  const { productId } = useParams();
+  const { data, loading } = useQuery({
+    queryFn: () => getProduct({ productId }),
+  });
+  if (!data || loading) return <Loading />;
+  const formattedDate = getFormattedDate(data?.createdAt);
   return (
     <section className={styles.section}>
       <div className={styles.imgWrapper}>
-        <ItemImg imgUrl='https://avatars.githubusercontent.com/u/31947480?v=4' />
+        <ItemImg imgUrl={data.images[0]} />
       </div>
       <div className={styles.contentsContainer}>
         <div className={styles.texts}>
           <hgroup className={styles.titles}>
-            <h1 className={styles.title}>아이패드 미니 팔아요</h1>
-            <h2 className={styles.price}>500,000원</h2>
+            <h1 className={styles.title}>{data.name}</h1>
+            <h2 className={styles.price}>
+              {data.price.toLocaleString('ko-KR') + '원'}
+            </h2>
             <button>
               <KebabIcon />
             </button>
           </hgroup>
           <div className={styles.infoContainer}>
             <h3 className={styles.infoLabel}>상품 소개</h3>
-            <article className={styles.description}>
-              액정에 잔기스랑 주변부 스크래치있습니다만 예민하신분아니면 전혀
-              신경쓰이지않을정도입니다. 박스 보관중입니다. 메모용과
-              넷플릭스용으로만쓰던거라 뭘 해보질 않아 기능이나 문제점을
-              못느꼈네요 잘 안써서 싸게넘깁니다! 택배거래안합니다.
-            </article>
+            <article className={styles.description}>{data.description}</article>
             <h3 className={styles.infoLabel}>상품 태그</h3>
             <div className={styles.tags}>
-              <Tag>아이패드미니</Tag>
-              <Tag>아이패드미니</Tag>
-              <Tag>아이패드미니</Tag>
+              {data.tags.map((tag) => (
+                <Tag key={`${crypto.randomUUID()}-${tag}`}>{tag}</Tag>
+              ))}
             </div>
           </div>
         </div>
         <div className={styles.sectionFooter}>
           <AuthorInfo
             variant={AUTHOR_INFO_VARIANTS.product}
-            nickname={'총명한 판다'}
-            updateAt={'2024. 01. 02'}
+            nickname={data.ownerNickname}
+            updateAt={formattedDate}
           />
           <div className={styles.likeButtonWrapper}>
             <button className={styles.likeButton}>
               <LikeIcon />
-              <span className={styles.count}>123</span>
+              <span className={styles.count}>{data.favoriteCount}</span>
             </button>
           </div>
         </div>
